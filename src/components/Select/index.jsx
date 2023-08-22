a096d628ea7e46dcd4060a2eeb6e866fa8f3ac8e
Wadson Vaval <vavalson@hotmail.com>
2023-08-21 23:00:49 -0400

    modified select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { View, TouchableOpacity } from 'react-native'
-import Text from './Text'
+import Text from '../Text'
 // import Screen from './screen'
-import { ThemeContext } from '../context/theme.context'
+import { ThemeContext } from '../../context/theme.context'
 
 // Custom import
-import tw from '../lib/tailwind'
-import Modal from '../components/Modal'
+import tw from '../../lib/tailwind'
+import Modal from '../Modal'
+import Option from './Option'
 
 export default function Select({
   items,
@@ -16,22 +17,30 @@ export default function Select({
   onValueChange,
   style,
   value,
+  LeftIcon,
   ...otherProps
 }) {
   const [selectedItem, setSelectedItem] = React.useState(value || null)
   const [modalVisible, setModalVisible] = React.useState(false)
   const themeContext = React.useContext(ThemeContext)
-  const col =
+  const themeColor =
     themeContext.theme === 'light'
       ? { backgroundColor: tw.color('gray-100') }
       : { backgroundColor: 'white' }
   return (
-    <View style={[tw`rounded-lg    p-3 my-1`, col, style]} {...otherProps}>
+    <View
+      style={[tw`rounded-lg    p-3 my-1`, themeColor, style]}
+      {...otherProps}
+    >
       <View style={tw`flex flex-row items-center justify-between`}>
-        <MaterialCommunityIcons name="apps" size={20} />
+        {LeftIcon ? (
+          <LeftIcon />
+        ) : (
+          <MaterialCommunityIcons name="apps" size={20} />
+        )}
         <View>
           {selectedItem ? (
-            <Text style={tw`font-semibold text-lg`}>{selectedItem}</Text>
+            <Text style={tw`font-semibold text-lg`}>{selectedItem.text}</Text>
           ) : (
             <Text style={tw`text-gray-400  text-lg`}>
               {placeholder || 'Pick an option'}
@@ -42,24 +51,22 @@ export default function Select({
           <MaterialCommunityIcons name="chevron-down" size={20} />
         </TouchableOpacity>
       </View>
+
       <Modal visible={modalVisible} OnClose={() => setModalVisible(false)}>
         <View style={tw`p-3`}>
           <Text style={tw`font-bold text-lg text-cs-primary my-2`}>
             Pick an option
           </Text>
           {items.map((item) => (
-            <TouchableOpacity
-              key={item.id || item.toString()}
-              onPress={() => {
+            <Option
+              key={item.id || item.value}
+              style={themeColor}
+              {...item}
+              onPress={(item) => {
                 setSelectedItem(item)
-                onValueChange(item)
                 setModalVisible(false)
               }}
-            >
-              <View style={tw.style(`my-1 p-3 rounded-lg`, col)}>
-                <Text style={tw`font-bold text-lg`}>{item}</Text>
-              </View>
-            </TouchableOpacity>
+            />
           ))}
         </View>
       </Modal>
